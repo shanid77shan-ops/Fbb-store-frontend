4000cca20431cd0dafd96b3083490c99f4e45cc3
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-01-09 01:31:15 +0530

    changes made in ui

--- src/Constant/Base.ts
+++ src/Constant/Base.ts
@@ -1,3 +1,3 @@
 // export const baseurl = "https://www.flybuybrand.com/api"
-export const baseurl = "http://localhost:3000/api"
-// export const baseurl = " https://fbb-store-backend.onrender.com/api"
+// export const baseurl = "http://localhost:3000/api"
+export const baseurl = " https://fbb-store-backend.onrender.com/api"
