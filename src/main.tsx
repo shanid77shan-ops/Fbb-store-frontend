a111b5a3a834cf9e72f18038b4f1c12d0169ad55
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-01-12 19:18:09 +0530

    use env variable for google oauth client id

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
-    <GoogleOAuthProvider clientId="418114033455-nu2odpsacsi4vb0sjrgsoebqcpfb9ai9.apps.googleusercontent.com">
+
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
