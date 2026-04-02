290d2cda8109739aba4dd0fe22c081f628fce2a3
shanid <shanid77shan@gmail.com>
2026-04-03 03:04:20 +0530

    zz

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-<<<<<<< HEAD
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID_HERE">
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);
-=======
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
 
@@ -23,4 +14,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
->>>>>>> 6f4220bdf6e446d714f6ce8799392dc31ec929ae
